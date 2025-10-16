91de5f0207e3c5a26747f9d6c07762c31608efc2
CRodrigo47 <christian3884@gmail.com>
2025-10-16 02:17:12 +0200

    Dark Mode Configured

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, DOCUMENT, Inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('trello-frontend');
+
+  public isDarkMode = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document){}
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      this.document.documentElement.classList.add('dark');
+    } else if (savedTheme === 'light') {
+      this.isDarkMode = false;
+      this.document.documentElement.classList.remove('dark');
+    } else {
+      this.isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      if (this.isDarkMode) {
+        this.document.documentElement.classList.add('dark');
+      }
+    }
+  }
+
+  public toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      this.document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
